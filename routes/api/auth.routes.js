738ebb53b44e6266aec724f007af98a6339dea9d
Eduard Batiushchenko <72995778+Eduard-Batiushchenko@users.noreply.github.com>
2021-06-09 11:22:23 +0300

    Auth. Registration logic done

--- routes/api/auth.routes.js
+++ routes/api/auth.routes.js
@@ -1,3 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const {
+  createNewUser,
+} = require('../../service/routes-services/auth/auth.controller.js')
+
+router.post('/registration', createNewUser)
 module.exports = router
