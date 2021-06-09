f846b0f87e9e47ed1e9a8c959be23456d0f2e8c4
Eduard Batiushchenko <72995778+Eduard-Batiushchenko@users.noreply.github.com>
2021-06-09 18:10:48 +0300

    Products. Change products name issue to products unique categories name

--- service/routes-services/products/products.methods.js
+++ service/routes-services/products/products.methods.js
@@ -1,12 +1,16 @@
 const Product = require('../../schemas/products')
 
 const findProductsByBlood = async bloodType => {
-  const result = await Product.find({ groupBloodNotAllowed: { $in: [true] } })
-  return result.reduce((acc, product) => {
+  const findProducts = await Product.find({
+    groupBloodNotAllowed: { $in: [true] },
+  })
+
+  const productsCategories = findProducts.reduce((acc, product) => {
     return product.groupBloodNotAllowed[bloodType] === true
-      ? [...acc, product.title.ru]
+      ? [...acc, product.categories[0]]
       : acc
   }, [])
+  return Array.from(new Set(productsCategories))
 }
 
 module.exports = findProductsByBlood
